a4ea967b4da2edcf2364a4dce5070ffab162dc65
debatableoutcome <dustypie@yandex.ru>
2023-06-26 23:49:53 +0600

    Ref: Listeners, bug fix: buttons flips active/nonactive, scroll fix

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ import utils from "./utils.mjs";
 function renderSet(set, mode = "consecutive") {
   console.log(set);
   const obj = state.dictionary.filter((obj) => obj.set === set);
+  console.log(obj[0].set);
   const pairs = obj[0].cards;
+
   if (mode === "consecutive") {
     return renderTape(pairs, set);
   }
   if (mode === "random") {
     const randomPairs = utils.shuffle(pairs);
-    console.log(randomPairs);
     return renderTape(randomPairs, set);
   }
 }
@@ -43,6 +44,7 @@ function renderTape(pairs, set) {
     div.classList.remove("invisible");
     dom.tape.appendChild(div);
     dom.tape.classList.remove("invisible");
+    console.log(set);
     dom.tape.dataset.set = set;
   });
   // Wake flip buttons up
@@ -62,7 +64,6 @@ function renderSets(sets) {
   });
 }
 
-function renderError() {}
 function renderSetsList() {
   const sets = state.dictionary.map((set) => set.set);
   dom.parentSets.innerHTML = "";
@@ -89,7 +90,23 @@ function timerDisplayAnswer(question, answer, ms) {
   }, ms);
 }
 
+function displayToggleTimer(event) {
+  const textBox = event.target.closest(".text-box");
+  const answer = textBox.querySelector(".answer");
+  const question = textBox.querySelector(".question");
+
+  if (answer.classList.contains("invisible")) {
+    answer.classList.remove("invisible");
+    question.classList.add("invisible");
+    timerDisplayAnswer(question, answer, 3000);
+  } else {
+    answer.classList.add("invisible");
+    question.classList.remove("invisible");
+  }
+}
+
 //&&&&&&&&&&&&&&&&&&&&&&& MANIPULATIONS &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
 const addSet = (set) => {
   const obj = {
     set,
@@ -105,13 +122,43 @@ const deleteSet = (set) => {
 };
 
 const addCard = (question, answer, set) => {
-  console.log(question, answer, set);
   const index = state.dictionary.findIndex((s) => s.set === set);
   const pair = [question, answer];
   state.dictionary[index].cards.push(pair);
 };
 
-function deleteCard(pair, set) {
+function submitNewCard() {
+  const q = dom.inputQ.value.toLowerCase();
+  const a = dom.inputA.value.toLowerCase();
+  const set = dom.dropdown.value.toLowerCase();
+  dom.leftBox.classList.add("invisible");
+  dom.leftBoxAlt.classList.add("invisible");
+  dom.rightBox.classList.remove("invisible");
+  dom.inputQ.value = "";
+  dom.inputA.value = "";
+  addCard(q, a, set);
+  dom.displayNotice("The card successfully added!");
+}
+
+function submitNewSet() {
+  const set = dom.inputSet.value.toLowerCase();
+  dom.inputSet.value = "";
+
+  dom.leftBox.classList.add("invisible");
+  dom.leftBoxAlt.classList.add("invisible");
+  dom.rightBox.classList.remove("invisible");
+
+  addSet(set);
+  dom.displayNotice("The set successfully added!");
+}
+
+function deleteCard(event) {
+  const target = event.target;
+  const set = dom.tape.dataset.set;
+  const textBox = target.closest(".text-box");
+  const question = textBox.querySelector(".question").textContent;
+  const answer = textBox.querySelector(".answer").textContent;
+  const pair = [question, answer];
   // Find the set
   const indexOfSet = state.dictionary.findIndex((s) => s.set === set);
 
@@ -119,11 +166,14 @@ function deleteCard(pair, set) {
   state.dictionary[indexOfSet].cards = state.dictionary[
     indexOfSet
   ].cards.filter((i) => !(i[0] === pair[0] && i[1] === pair[1]));
+  // hide card
+
+  textBox.classList.add("invisible");
+  dom.cardBody.scrollBy(0, 200);
 }
 
 //-------------------------
 function replacePair(obj) {
-  console.log(obj);
   // find the required set
   const indexOfSet = state.dictionary.findIndex((s) => s.set === obj.set);
   // find the edited pair
@@ -135,213 +185,50 @@ function replacePair(obj) {
   state.dictionary[indexOfSet].cards[toBeReplaced][1] = obj.newB;
 }
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%  Event Listeners  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-window.addEventListener("DOMContentLoaded", () => {
-  renderSetsList();
-});
-
-// Notifications (dialogs)
-dom.body.addEventListener("click", function (event) {
+function removeDisable(event) {
   const target = event.target;
-  // CLOSE DIALOG
-  if (target.classList.contains("close-dialog-btn")) {
-    console.log(target);
-    dom.closeDialog();
-  }
-  // DELETE SET CONFIRMATION
-  if (target.classList.contains("yes-delete-btn")) {
-    const set = target.parentElement.parentElement.dataset.set
-      .trim()
-      .toLowerCase();
-    deleteSet(set);
-    dom.closeDialog();
-    dom.displayNotice("The set has been permanently deleted");
-  }
-});
 
-dom.parentWrapper.addEventListener("click", function (event) {
+  if (
+    target.classList.contains("fieldA") ||
+    target.classList.contains("fieldB")
+  ) {
+    const btnLeft = document.querySelector(".arrow-left");
+    const btnRight = document.querySelector(".arrow-right");
+    dom.parentSets.classList.remove("disabled");
+    dom.rightBox.classList.remove("disabled");
+    btnLeft.classList.remove("disabled");
+    btnRight.classList.remove("disabled");
+  }
+}
+
+function addDisable(event) {
   const target = event.target;
-  console.log(target);
-  // SHIFT LEFT
   if (
-    target.classList.contains("arrow-left") ||
-    target.classList.contains("fa-chevron-left")
+    target.classList.contains("fieldA") ||
+    target.classList.contains("fieldB")
   ) {
-    dom.cardBody.scrollBy(0, -200);
+    const btnLeft = document.querySelector(".arrow-left");
+    const btnRight = document.querySelector(".arrow-right");
+    dom.parentSets.classList.add("disabled");
+    dom.rightBox.classList.add("disabled");
+    btnLeft.classList.add("disabled");
+    btnRight.classList.add("disabled");
   }
-  // SHIFT RIGHT
-  if (
-    target.classList.contains("arrow-right") ||
-    target.classList.contains("fa-chevron-right")
-  ) {
-    if (
-      dom.cardBody.scrollTop + dom.cardBody.clientHeight >=
-      dom.cardBody.scrollHeight
-    ) {
-      hideAnswer(target);
-      // Infinite scroll to the right
-      dom.cardBody.scrollTop = 0;
-    } else {
-      dom.cardBody.scrollBy(0, 200);
-    }
-  }
-  if (target.classList.contains("set")) {
-    const textEl = target.closest(".set");
-    const set = textEl.textContent.toLowerCase().trim();
-    return renderSet(set);
-  }
+}
 
-  if (
-    // TOGGLE answer / quesion with timer
-    target.classList.contains("text-box") ||
-    target.classList.contains("question") ||
-    target.classList.contains("answer")
-  ) {
-    console.log(target);
-    const textBox = target.closest(".text-box");
-    const answer = textBox.querySelector(".answer");
-    const question = textBox.querySelector(".question");
+function confirmSetDeletion(event) {
+  const set = event.target.parentElement.parentElement.dataset.set
+    .trim()
+    .toLowerCase();
+  deleteSet(set);
+  dom.closeDialog();
+  dom.displayNotice("The set has been permanently deleted");
+}
 
-    if (answer.classList.contains("invisible")) {
-      answer.classList.remove("invisible");
-      question.classList.add("invisible");
-      timerDisplayAnswer(question, answer, 3000);
-    } else {
-      answer.classList.add("invisible");
-      question.classList.remove("invisible");
-    }
-  }
-  // ADD CARD
-  if (target.classList.contains("btn-add-card")) {
-    console.log(target);
-    console.log(dom.leftBox.classList);
-    dom.leftBox.classList.remove("invisible");
-    console.log(dom.leftBox.classList);
-    dom.rightBox.classList.add("invisible");
-  }
-  // ADD SET
-  if (target.classList.contains("btn-add-set")) {
-    console.log(target);
-    dom.leftBoxAlt.classList.remove("invisible");
-    dom.rightBox.classList.add("invisible");
-  }
-  // CLOSE ADD SET BOX
-  if (target.classList.contains("fa-xmark")) {
-    console.log(target);
-    dom.leftBoxAlt.classList.add("invisible");
-    dom.leftBox.classList.add("invisible");
-    dom.rightBox.classList.remove("invisible");
-  }
-  // DELETE SET
-  if (
-    target.classList.contains("set-del") ||
-    target.classList.contains("btn-set-delete")
-  ) {
-    console.log(target);
-    const div = target.closest(".set");
-    const set = div.textContent;
-    dom.displayConfirmationDelSet(set.trim().toLowerCase());
-  }
-  // DELETE CARD
-  if (
-    target.classList.contains("card-del") ||
-    target.classList.contains("btn-delete-card")
-  ) {
-    console.log(target);
-    const set = dom.tape.dataset.set;
-    const textBox = target.closest(".text-box");
-    console.log(textBox);
-    const question = textBox.querySelector(".question").textContent;
-    const answer = textBox.querySelector(".answer").textContent;
-    const pair = [question, answer];
-    deleteCard(pair, set);
-    // hide card
-    textBox.classList.add("invisible");
-    dom.cardBody.scrollBy(0, 200);
-  }
-  // EDIT CARD
-  if (
-    target.classList.contains("btn-edit") ||
-    target.classList.contains("fa-pen-to-square")
-  ) {
-    // Retrieve the text
-    const parent = target.closest(".text-box");
-    const q = parent.querySelector(".question");
-    const a = parent.querySelector(".answer");
-    console.log(parent, q, a);
-    const textA = q.textContent.trim();
-    const textB = a.textContent.trim();
-    console.log(textA, textB);
-
-    // Create (clone the template and fill in) the form
-    const form = dom.formEditTemplate.cloneNode(true);
-    // Take text A and text B before editting
-    const fieldA = form.querySelector(".fieldA");
-    fieldA.value = textA;
-    const fieldB = form.querySelector(".fieldB");
-    fieldB.value = textB;
-    const prevValues = String(textA, `:`, textB);
-
-    form.classList.remove("invisible");
-    form.classList.remove("form-edit-template");
-
-    form.dataset.original = String(prevValues);
-
-    // Hide All
-    dom.tape.classList.add("invisible");
-    // Nest the Form
-    dom.cardBody.appendChild(form);
-  }
-  // RANDOMIZE
-  if (
-    target.classList.contains("btn-random") ||
-    target.classList.contains("fa-shuffle")
-  ) {
-    const set = dom.tape.dataset.set;
-    renderSet(set, "random");
-  }
-});
-
-// FORM SUBMIT FOR ADD CARD OR SET
-dom.addCardForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const submitType = event.submitter.value;
-  if (submitType === "add-card") {
-    if (dom.inputQ.value === "" || dom.inputA.value === "") return;
-
-    const q = dom.inputQ.value.toLowerCase();
-    const a = dom.inputA.value.toLowerCase();
-    const set = dom.dropdown.value.toLowerCase();
-    dom.leftBox.classList.add("invisible");
-    dom.leftBoxAlt.classList.add("invisible");
-    dom.rightBox.classList.remove("invisible");
-    dom.inputQ.value = "";
-    dom.inputA.value = "";
-    addCard(q, a, set);
-    dom.displayNotice("The card successfully added!");
-  }
-
-  if (submitType === "add-set") {
-    if (dom.inputSet.value === "") return;
-
-    const set = dom.inputSet.value.toLowerCase();
-    dom.inputSet.value = "";
-
-    dom.leftBox.classList.add("invisible");
-    dom.leftBoxAlt.classList.add("invisible");
-    dom.rightBox.classList.remove("invisible");
-
-    addSet(set);
-    dom.displayNotice("The set successfully added!");
-  }
-});
-
-// FORM SUBMIT FOR CARD EDITS
-dom.cardBody.addEventListener("submit", (event) => {
+function editsSubmit(event) {
   const target = event.target;
   event.preventDefault();
-  console.log("hello submit");
+
   // retrieving data to be edited
   const fieldA = target.parentElement.querySelector(".fieldA");
   const fieldB = target.parentElement.querySelector(".fieldB");
@@ -359,41 +246,173 @@ dom.cardBody.addEventListener("submit", (event) => {
   replacePair(editedAB);
   formEdits.classList.add("invisible");
   renderSet(dom.tape.dataset.set);
+}
+
+function editCard(event) {
+  // Retrieve the text
+  const parent = event.target.closest(".text-box");
+  const q = parent.querySelector(".question");
+  const a = parent.querySelector(".answer");
+  console.log(parent, q, a);
+  const textA = q.textContent.trim();
+  const textB = a.textContent.trim();
+  console.log(textA, textB);
+
+  // Create (clone the template and fill in) the form
+  const form = dom.formEditTemplate.cloneNode(true);
+  // Take text A and text B before editting
+  const fieldA = form.querySelector(".fieldA");
+  fieldA.value = textA;
+  const fieldB = form.querySelector(".fieldB");
+  fieldB.value = textB;
+  const prevValues = String(textA, `:`, textB);
+
+  form.classList.remove("invisible");
+  form.classList.remove("form-edit-template");
+
+  form.dataset.original = String(prevValues);
+
+  // Hide All
+  dom.tape.classList.add("invisible");
+  // Nest the Form
+  dom.cardBody.appendChild(form);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%  Event Listeners  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+window.addEventListener("DOMContentLoaded", () => {
+  renderSetsList();
 });
+
+// Notifications (dialogs)
+dom.body.addEventListener("click", function (event) {
+  const target = event.target;
+  // CLOSE DIALOG
+  if (target.classList.contains("close-dialog-btn")) {
+    dom.closeDialog();
+  }
+  // DELETE SET CONFIRMATION
+  if (target.classList.contains("yes-delete-btn")) {
+    confirmSetDeletion(event);
+  }
+});
+
+dom.parentWrapper.addEventListener("click", function (event) {
+  const target = event.target;
+  // SHIFT LEFT
+  if (
+    target.classList.contains("arrow-left") ||
+    target.classList.contains("fa-chevron-left")
+  ) {
+    dom.cardBody.scrollBy(0, -200);
+  }
+  // SHIFT RIGHT
+  if (
+    target.classList.contains("arrow-right") ||
+    target.classList.contains("fa-chevron-right")
+  ) {
+    if (
+      dom.cardBody.scrollTop + dom.cardBody.clientHeight >=
+      dom.cardBody.scrollHeight
+    ) {
+      // hideAnswer(target);
+      // Infinite scroll to the right
+      dom.cardBody.scrollTop = 0;
+    } else {
+      dom.cardBody.scrollBy(0, 200);
+    }
+  }
+  if (target.classList.contains("set")) {
+    const textEl = target.closest(".set");
+    const set = textEl.textContent.toLowerCase().trim();
+
+    return renderSet(set);
+  }
+
+  if (
+    // TOGGLE answer / quesion with timer
+    target.classList.contains("text-box") ||
+    target.classList.contains("question") ||
+    target.classList.contains("answer")
+  ) {
+    displayToggleTimer(event);
+  }
+
+  // ADD CARD
+  if (target.classList.contains("btn-add-card")) {
+    dom.leftBox.classList.remove("invisible");
+    dom.rightBox.classList.add("invisible");
+  }
+  // ADD SET
+  if (target.classList.contains("btn-add-set")) {
+    dom.leftBoxAlt.classList.remove("invisible");
+    dom.rightBox.classList.add("invisible");
+  }
+  // CLOSE ADD SET BOX
+  if (target.classList.contains("fa-xmark")) {
+    dom.leftBoxAlt.classList.add("invisible");
+    dom.leftBox.classList.add("invisible");
+    dom.rightBox.classList.remove("invisible");
+  }
+  // DELETE SET
+  if (
+    target.classList.contains("set-del") ||
+    target.classList.contains("btn-set-delete")
+  ) {
+    const div = target.closest(".set");
+    const set = div.textContent;
+    dom.displayConfirmationDelSet(set.trim().toLowerCase());
+  }
+  // DELETE CARD
+  if (
+    target.classList.contains("card-del") ||
+    target.classList.contains("btn-delete-card")
+  ) {
+    deleteCard(event);
+  }
+  // EDIT CARD
+  if (
+    target.classList.contains("btn-edit") ||
+    target.classList.contains("fa-pen-to-square")
+  ) {
+    editCard(event);
+  }
+  // RANDOMIZE
+  if (
+    target.classList.contains("btn-random") ||
+    target.classList.contains("fa-shuffle")
+  ) {
+    const set = dom.tape.dataset.set;
+    renderSet(set, "random");
+  }
+});
+
+// FORM SUBMIT FOR ADD CARD OR SET
+dom.addCardForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const submitType = event.submitter.value;
+
+  if (submitType === "add-card") {
+    if (dom.inputQ.value === "" || dom.inputA.value === "") return;
+    submitNewCard();
+  }
+
+  if (submitType === "add-set") {
+    if (dom.inputSet.value === "") return;
+    submitNewSet();
+  }
+});
+
+// FORM SUBMIT FOR CARD EDITS
+dom.cardBody.addEventListener("submit", (event) => editsSubmit(event));
 
 // FORM FOCUS/FOCUSOUT
 // FORMS ACTIVE - REST BLOCKED
-dom.parentWrapper.addEventListener("focusin", (event) => {
-  const target = event.target;
-  console.log(target);
-  if (
-    target.classList.contains("fieldA") ||
-    target.classList.contains("fieldB")
-  ) {
-    const btnLeft = document.querySelector(".arrow-left");
-    const btnRight = document.querySelector(".arrow-right");
-    dom.parentSets.classList.add("disabled");
-    dom.rightBox.classList.add("disabled");
-    btnLeft.classList.add("disabled");
-    btnRight.classList.add("disabled");
-  }
-});
+dom.parentWrapper.addEventListener("focusin", (event) => addDisable(event));
+
 // FORMS NONACTIVE - REST ACTIVE
-dom.parentWrapper.addEventListener("focusout", (event) => {
-  const target = event.target;
-  console.log(target);
-  if (
-    target.classList.contains("fieldA") ||
-    target.classList.contains("fieldB")
-  ) {
-    const btnLeft = document.querySelector(".arrow-left");
-    const btnRight = document.querySelector(".arrow-right");
-    dom.parentSets.classList.remove("disabled");
-    dom.rightBox.classList.remove("disabled");
-    btnLeft.classList.remove("disabled");
-    btnRight.classList.remove("disabled");
-  }
-});
+dom.parentWrapper.addEventListener("focusout", (event) => removeDisable(event));
+
 // CANCEL CARD EDITS
 dom.btnCancelEdits.addEventListener("click", (event) => {
   const form = event.target.closest(".form-edit");
@@ -407,21 +426,20 @@ dom.btnCancelEdits.addEventListener("click", (event) => {
 // FEATURES:
 
 // local storage
-// errors
 
 //UI:
 
 // If the list is empty display This set is empty + a button to add a card: ok cancel
 
 //BUGS:
+// display answer by clicking on the textbox
 // Submit edits card - fix size of a field
 
-// Notice is to style cute
-// buttons flip should be only active when the set is on *
+// Notice to style cute
 // adding a card, then cancel - clean fields before
 
 // REF:
 
 //DONE TODAY:
-// size of text div
-// overflow
+// Listeners
+// buttons flip should be only active when the set is on *
